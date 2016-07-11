a46f1f2752c5ba3b7e7ce754c29ee8d23297e6f4
eden_tuan <atuanvptd@gmail.com>
2016-07-11 18:51:35 +0700

    OWL carousel!

--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -6,45 +6,36 @@ $(document).ready(function(){
 
 
   //owlCarousel Js ---------------------------------------------
-     $("#owl-demo").owlCarousel({
- 
-        navigation : true, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
+     var owl = $("#owl-demo");
    
-        // "singleItem:true" is a shortcut for:
-        // items : 1, 
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
+      owl.owlCarousel({
+          items : 1, //10 items above 1000px browser width
+          itemsDesktop : [1000,1], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,1], // betweem 900px and 601px
+          itemsTablet: [600,1], //2 items between 600 and 0
+          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+      });
+     
+      // Custom Navigation Events
+      $(".next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".prev").click(function(){ 
+        owl.trigger('owl.prev');
+      })
+      $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+      })
+      $(".stop").click(function(){
+        owl.trigger('owl.stop');
+      })
    
-    });
+  
    //owlCarousel Js ---------------------------------------------
 
 
 	 // external js: isotope.pkgd.js------------------------------------
-    $(".grid").isotope({
-           itemSelector: '.grid-item-2',
-           layoutMode: 'fitRows',
-        });
 
-        $('.portfolio-list li').click(function(){
-          $('.portfolio-list li').removeClass('active');
-          $(this).addClass('active');
-
-          var selector =$(this).attr('data-filter');
-          $(".grid").isotope({
-            filter: selector,
-            animationOption: {
-              duration: 750,
-              easing: 'linear',
-              queue: false,
-            }
-          });
-            return false;
-        });
   	// End external js: isotope.pkgd.js---------------------------------
 
     //Slider show -js------------------------------------
@@ -83,6 +74,7 @@ $(document).ready(function(){
       //  }
      
     // End slideShow-js------------------------------------
+
 
 
 });
