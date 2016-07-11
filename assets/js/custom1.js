4148eed5b3a3e9c32d94aefb855f058d9cea47e6
eden_tuan <atuanvptd@gmail.com>
2016-07-11 16:55:49 +0700

    Complete Slideshow ....

--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -1,74 +1,88 @@
-console.log('script from cutsom1');
+console.log('script from cutsom11111111');
 
 
 
 $(document).ready(function(){
 
-	// Smoot-sCroll Js ---------------------------------------------
-	// $(function(){ 
-
-	// 	 var $window = $(window);
-	// 	 var scrollTime = 0.8;
-	// 	 var scrollDistance = 300;
-
-	// 	 $window.on("mousewheel DOMMouseScroll", function(event){
-
-	// 	  event.preventDefault(); 
-
-	// 	  var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-	// 	  var scrollTop = $window.scrollTop();
-	// 	  var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-
-	// 	  TweenMax.to($window, scrollTime, {
-	// 	   scrollTo : { y: finalScroll, autoKill:true },
-	// 	    ease: Power1.easeOut,
-	// 	    overwrite: 5       
-	// 	   });
-
-	// 	 });
-	// 	});
-	//End Smoot-sCroll Js --------------------------------------------
 
   //owlCarousel Js ---------------------------------------------
-	$('.nav li').click(function(){
-		$('.nav li').removeClass('active');
-		$(this).addClass('active');
-	});
-
-	 $("#owl-demo").owlCarousel({
+     $("#owl-demo").owlCarousel({
  
-	      autoPlay: false, //Set AutoPlay to 3 seconds
-	 
-	      items : 4,
-	      itemsDesktop : [1199,3],
-	      itemsDesktopSmall : [979,3]
-	 
-	  });
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+   
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+   
+    });
    //owlCarousel Js ---------------------------------------------
 
 
 	 // external js: isotope.pkgd.js------------------------------------
-      $(".grid").isotope({
-         itemSelector: '.grid-item-2',
-         layoutMode: 'fitRows',
-      });
-
-      $('.portfolio-list li').click(function(){
-        $('.portfolio-list li').removeClass('active');
-        $(this).addClass('active');
-
-        var selector =$(this).attr('data-filter');
-        $(".grid").isotope({
-          filter: selector,
-          animationOption: {
-            duration: 750,
-            easing: 'linear',
-            queue: false,
-          }
+    $(".grid").isotope({
+           itemSelector: '.grid-item-2',
+           layoutMode: 'fitRows',
         });
-          return false;
-      });
-  	// End external js: isotope.pkgd.js---------------------------------	
-   
+
+        $('.portfolio-list li').click(function(){
+          $('.portfolio-list li').removeClass('active');
+          $(this).addClass('active');
+
+          var selector =$(this).attr('data-filter');
+          $(".grid").isotope({
+            filter: selector,
+            animationOption: {
+              duration: 750,
+              easing: 'linear',
+              queue: false,
+            }
+          });
+            return false;
+        });
+  	// End external js: isotope.pkgd.js---------------------------------
+
+    //Slider show -js------------------------------------
+      var myIndex = 0;
+      carousel();
+     
+      function carousel() {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          for (i = 0; i < x.length; i++) {
+             x[i].style.display = "none";
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1;}
+          if (myIndex < 1) {myIndex = x.length;}
+          x[myIndex-1].style.display = "block";
+          time = setTimeout(carousel, 4000); // Change image every 2 seconds
+      }
+
+      // function next(){
+         
+      //  }
+      $('.prev-slideshow').click(function(event) {
+           clearTimeout(time);
+           $('.process').animate({width: '0%'}, 3500);
+           myIndex = $('.mySlides:visible').index() - 1 ;
+           carousel() ;
+       });
+       $('.next-slideshow').click(function(event) {
+           clearTimeout(time);
+           myIndex = $('.mySlides:visible').index() + 1 ;
+           carousel() ;
+       });
+      // function prev(){
+         
+      //  }
+     
+    // End slideShow-js------------------------------------
+
 
 });
